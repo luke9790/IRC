042d23252f1dc8e42497362cd8647ba528365265
Diego Molinari <diego@diego.lan>
2024-02-16 14:29:55 +0100

    modeProto

--- Diego/Channel.cpp
+++ Diego/Channel.cpp
@@ -1,6 +1,26 @@
 #include "Channel.hpp"
 
-Channel::Channel(std::string channelName) : name(channelName), topic("") {}
+Channel::Channel(std::string channelName, int fd) : name(channelName), topic(""), userCount(0), topic_mode(false) {
+    setChannelOperator(fd);
+}
+
+void Channel::setChannelOperator(int fd_op)
+{
+    operators.push_back(fd_op);
+}
+
+void Channel::removeChannelOperator(int fd_op)
+{
+    std::vector<int>::iterator it;
+    for(it = operators.begin(); it != operators.end(); it++)
+    {
+        if (*it == fd_op)
+        {
+            it = operators.erase(it);
+            break;
+        }
+    }
+}
 
 Channel::~Channel() {
     // Pulizia delle risorse, se necessario
@@ -12,6 +32,8 @@ void Channel::addClient(Client* client) {
             return; // Il client è già nel canale
         }
     }
+    client->setChannel(this->name);
+    userCount++;
     clients.push_back(client); // Aggiungi il client al canale
 }
 
@@ -19,7 +41,37 @@ void Channel::removeClient(Client* client) {
     for (std::vector<Client*>::iterator it = clients.begin(); it != clients.end(); ++it) {
         if (*it == client) {
             clients.erase(it); // Rimuovi il client dal canale
+            userCount--;
             break;
+        }
+    }
+}
+
+
+bool Channel::isOperator(int client_fd)
+{
+    for (std::vector<int>::iterator it = operators.begin(); it != operators.end(); ++it) {
+        if (client_fd == *it)
+            return true;
+    }
+    return false;
+}
+
+bool Channel::isClientInChannel(int client_fd) const {
+    for (size_t i = 0; i < clients.size(); ++i) {
+        if (clients[i]->socket_fd == client_fd) {
+            return true; // Il client è presente nel canale
+        }
+    }
+    return false; // Il client non è stato trovato nel canale
+}
+
+void Channel::broadcast(const std::string& message) {
+    for (std::vector<Client*>::iterator it = clients.begin(); it != clients.end(); ++it) {
+        Client* client = *it;
+        // Controlla se il client corrente deve essere escluso dalla broadcast
+        if (client->socket_fd != -1) {
+            send(client->socket_fd, message.c_str(), message.length(), 0);
         }
     }
 }
@@ -40,6 +92,46 @@ const std::string& Channel::getTopic() const {
     return topic;
 }
 
-std::vector<Client*> Channel::getClients() const {
+std::vector<Client*> Channel::getClients() const{
     return clients;
+}
+
+int Channel::getUserCount() const
+{
+    return userCount;
+}
+
+void Channel::setMode_t(bool flg)
+{
+    topic_mode = flg;
+}
+
+void Channel::setInviteOnly(bool value)
+{
+    invite_only = value;
+}
+
+void Channel::setUserLimits(int limits)
+{
+    user_limits = limits;
+}
+
+void Channel::setPassword(std::string pass)
+{
+    pwd = pass;
+}
+
+int Channel::getUserLimits()
+{
+    return user_limits;
+}
+
+std::string Channel::getPassword()
+{
+    return pwd;
+}
+
+bool Channel::getInviteOnly()
+{
+    return invite_only;
 }
--- Diego/IRCServ.cpp
+++ Diego/IRCServ.cpp
@@ -29,12 +29,6 @@ IRCServ::IRCServ(int port, const std::string& password) : port(port), password(p
         close(server_fd);
         throw std::runtime_error("Socket listen failed");
     }
-    // Inizializza i canali predefiniti
-    channels["generale"] = new Channel("generale");
-    channels["programmazione"] = new Channel("programmazione");
-    // Se desideri, puoi anche impostare topic predefiniti qui
-    channels["generale"]->setTopic("Benvenuti nel canale generale");
-    channels["programmazione"]->setTopic("Benvenuti nel canale di programmazione");
 }
 
 IRCServ::~IRCServ() {
@@ -86,7 +80,7 @@ void IRCServ::run() {
                         clients[new_client_fd] = new Client(new_client_fd); // Add to clients map
                         FD_SET(new_client_fd, &master_set); // Add to master set
                         if (new_client_fd > max_fd)
-                            max_fd = new_client_fd; // Update max if needed
+                        max_fd = new_client_fd; // Update max if needed
                     }
                 }
                 else // significa che abbiamo dati da un client esistente
